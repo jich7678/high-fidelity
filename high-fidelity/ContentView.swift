adf1ec7385a09c92c857bd0f77202c5c079f4cd7
Jiawei Chen <jich7678@colorado.edu>
2023-11-30 16:15:57 -0700

    checkpointo

--- high-fidelity/ContentView.swift
+++ high-fidelity/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showLoginView = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Text("SOLOGUARD")
+                    .font(.system(size: 40))
+                    .font(.headline)
+                    .fontWeight(.heavy)
+                    .padding()
+                
+                Image(systemName: "airplane.circle")
+                    .foregroundColor(.white)
+                    .font(.system(size: 100))
+                    .rotationEffect(.degrees(-45))
+                    .padding(.bottom, 80)
+        
+                    Button("Log in") {
+                        if (showLoginView == false) {
+                            showLoginView = true
+                        }
+                    }
+                    .foregroundColor(.black)
+                    .frame(width: 200, height: 50)
+                    .background(Color.yellow2)
+                    .cornerRadius(5)
+                    .shadow(radius: 5)
+                
+                    NavigationLink(destination: LoginView(), isActive: $showLoginView){
+                        EmptyView()
+                    }
+                
+                Text("")
+                
+                Button("Create an account") {
+                }
+                .foregroundColor(.black)
+                .frame(width: 200, height: 50)
+                .background(Color.yellow2)
+                .cornerRadius(5)
+                .shadow(radius: 5)
+                
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.perry)
         }
-        .padding()
     }
 }
 
