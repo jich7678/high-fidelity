b84258adfae9b0b7b03683fbdeea950c898653e4
Jiawei Chen <jich7678@colorado.edu>
2023-11-30 13:56:24 -0700

    Initial Commit

---
+++ high-fidelity/high_fidelityApp.swift
@@ -0,0 +1,17 @@
+//
+//  high_fidelityApp.swift
+//  high-fidelity
+//
+//  Created by Sam Chen on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct high_fidelityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
